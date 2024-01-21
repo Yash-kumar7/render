bcc14d3a9d76a12ec80a3c22a209002ece69bfee
Yash-kumar7 <149219028+Yash-kumar7@users.noreply.github.com>
2024-01-21 03:50:34 -0800

    Add files via upload

--- src/main/java/com/backend/service/UserService.java
+++ src/main/java/com/backend/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserService {
     void registerUser(UserDto userDto);
+    public void logoutUser(String userEmail);
     boolean loginUser(UserDto userDto);
     User findByEmail(String email);
 }
